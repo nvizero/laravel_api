d1cd89175dcb2ca4e8585e3a07d69914c02de89c
withvictor <withvictor@gmail.com>
2022-01-12 23:29:20 +0800

    add product attirt

--- app/Admin/Controllers/ProductsController.php
+++ app/Admin/Controllers/ProductsController.php
@@ -71,7 +71,8 @@ class ProductsController extends AdminController
         //     $form->select('category_style_id', 'size/大小/尺寸')->options($categorieStyles->pluck('name','id'))->rules('min:1');            
         // });
         $form->table('attrib','size/大小/尺寸-庫存', function ($table) use ($categorieStyles) {
-            $table->select('category_style_id', 'size/大小/尺寸')->options($categorieStyles->pluck('name','id'))->rules('min:1');                        
+            $table->select('category_style_id', 'size/大小/尺寸')->options($categorieStyles->pluck('name','id'))->rules('min:1');
+            $table->text('style', '顏色/型號');
             $table->text('num','庫存-數量');
             $table->text('price','價格');
             $table->multipleImage('image', __('圖片'))->move('uploads/images')->removable();
